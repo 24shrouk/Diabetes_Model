ee8494a82749f2bdeb7a3bf7a9349a3394b0bd29
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-08-27 23:43:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,30 @@
 import streamlit as st
+import pickle
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Load your model (adjust the path accordingly)
+model_path = 'model (1).pkl'
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)
+
+# Title of the app
+st.title("Diabetes Model Prediction")
+
+# Input features
+pregnancies = st.text_input("Enter Pregnancies:")
+Glucose = st.text_input("Enter Glucose :")
+insulin = st.text_input("Enter Insulin :")
+bmi = st.text_input("Enter BMI :")
+diabetesPedigreeFunction = st.text_input("Enter DiabetesPedigreeFunction :")
+age = st.text_input("Enter Age :")
+
+# Button for prediction
+if st.button("Predict"):
+    # Convert features to a numpy array
+    features = np.array([[float(pregnancies), float(Glucose), float(insulin),float(bmi),float(diabetesPedigreeFunction),float(age)]])
+    
+    # Make prediction
+    prediction = model.predict(features)
+    
+    # Display the prediction
+    st.write(f"Diabetes Prediction is : {prediction[0]}")
